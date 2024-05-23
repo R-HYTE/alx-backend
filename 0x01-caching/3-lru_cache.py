a06c9a2d837390cd07a21ae0b04223c841eac3b5
R_HYTE <polycarpwathuta39@gmail.com>
2024-05-23 23:47:37 +0300

    feat: Implement LRU Cache Module

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    This caching system implements the Least Recently Used eviction policy.
+    """
+
+    def __init__(self):
+        """Initialize the LRU cache."""
+        super().__init__()
+        self.access_order = []
+
+    def put(self, key, item):
+        """Add an item in the cache."""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.access_order.remove(key)
+
+        self.cache_data[key] = item
+        self.access_order.append(key)
+
+        if len(self.cache_data) > self.MAX_ITEMS:
+            lru_key = self.access_order.pop(0)
+            del self.cache_data[lru_key]
+            print("DISCARD:", lru_key)
+
+    def get(self, key):
+        """Get an item by key."""
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.access_order.remove(key)
+        self.access_order.append(key)
+
+        return self.cache_data[key]
