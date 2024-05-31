601236e2ba9b87e1d44dcb4d496dab4d5e828c3e
R_HYTE <polycarpwathuta39@gmail.com>
2024-06-01 00:24:40 +0300

    Included module description and docstrings

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,10 +1,23 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app with a single route.
+"""
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
+    """
+    Render the index page.
+
+    Returns:
+        The rendered HTML template for the index page.
+    """
     return render_template('0-index.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
