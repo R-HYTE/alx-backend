3365a169c2e85641b679cbbd52b89e87ed607e29
R_HYTE <polycarpwathuta39@gmail.com>
2024-06-02 23:27:58 +0300

    Parametrize templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""
+This app serves a simple HTML page with "Welcome to Holberton" as the page
+title and "Hello world" as the header. It supports internationalization with
+English and French languages.
+"""
+
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """
+    Config class for setting Babel configurations.
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale() -> str:
+    """
+    Determine the best match for the user's preferred language.
+
+    Returns:
+        str: The best-matched language code.
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    Render the index page.
+
+    Returns:
+        str: The rendered HTML template for the index page.
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
