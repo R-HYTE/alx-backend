f90d6a62fbbb7a71de2f6cc3d32ebf1ffe0a4563
R_HYTE <polycarpwathuta39@gmail.com>
2024-06-28 21:14:22 +0300

    feat: Implemented Redis client to subscribe to the 'holberton school channel', logging messages received and handling termination gracefully on SIGINT.

---
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -0,0 +1,31 @@
+import redis from 'redis';
+
+const client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe();
+    client.quit();
+    console.log('Subscriber unsubscribed and connection closed gracefully');
+  }
+});
+
+// Gracefully close Redis connection on process exit
+process.on('SIGINT', () => {
+  client.unsubscribe();
+  client.quit();
+  console.log('\nSubscriber unsubscribed and connection closed gracefully');
+  process.exit();
+});
